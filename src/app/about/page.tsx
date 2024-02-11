e89904078576b90be140a84f51baa5fcf45f3902
etnichols <e3t5n6@gmail.com>
2024-02-11 17:47:54 -0500

    fix build errors

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,22 +3,20 @@
 import { Section } from '@/components/section'
 
 export default function Home() {
+  const paragraphs = [
+    "It's the game you've probably played a thousand times with your friends: you're watching an NBA game, see a player you know, and ask you're friend, \"You guys know where he went to college??\"",
+    'This, my friends, is the "From???" game. And now, it\'s a website.',
+    'The game is simple: for every player shown, identify where they went to college.',
+    'Create quizzes, share with your friends, compare scores, earn your bragging rights.',
+  ]
+
   return (
     <Section headline="About From???">
-      <p className="leading-loose">
-        It's the game you've probably played a thousand times with your friends: you're watching an
-        NBA game, see a player you know, and ask you're friend, "You guys know where he went to
-        college??"
-      </p>
-      <p className="leading-loose">
-        This, my friends, is the "From???" game. And now, it's a website.
-      </p>
-      <p className="leading-loose">
-        The game is simple: for every player shown, identify where they went to college.
-      </p>
-      <p className="leading-loose">
-        Create quizzes, share with your friends, compare scores, earn your bragging rights.
-      </p>
+      {paragraphs.map((item, index) => (
+        <p key={index} className="leading-loose">
+          {item}
+        </p>
+      ))}
     </Section>
   )
 }
