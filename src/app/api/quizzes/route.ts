e0a6fc0b29c5056a53fe6117bc82377a1d59ebe3
etnichols <e3t5n6@gmail.com>
2024-03-22 00:20:56 -0400

    start leaderboard

---
+++ src/app/api/quizzes/route.ts
@@ -0,0 +1,23 @@
+import Quiz from '@/models/Quiz'
+import dbConnect from '@/lib/db-connect'
+
+export async function GET(request: Request, { params }: { params: { slug: string } }) {
+  await dbConnect()
+
+  const quizzes = await Quiz.find({})
+    .select({ title: 1, _id: 1 })
+    .sort({ createdAt: -1 }) // Sort by the createdAt field in descending order
+    .limit(10)
+
+  if (!quizzes) {
+    return new Response(
+      JSON.stringify({
+        message: 'Quizzes not found',
+        success: false,
+      }),
+      { status: 404 },
+    )
+  }
+
+  return new Response(JSON.stringify({ quizzes }), { status: 200 })
+}
--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,11 +1,38 @@
-'use client'
-
+import { Combobox } from '@/components/ui/combo-box'
 import { Section } from '@/components/ui/section'
 
-export default function Home() {
+export default async function Home() {
+  const quizzes = await fetchQuizzes()
+
+  const actualQuizzes = quizzes.quizzes.map((quiz) => ({
+    _id: quiz._id,
+    value: quiz._id,
+    label: quiz.title,
+  }))
+
   return (
     <Section headline="Leaderboard">
+      <Combobox items={actualQuizzes} />
       <div>Not yet implemented. Come back soon!</div>
     </Section>
   )
+}
+
+async function fetchQuizzes() {
+  try {
+    const apiResponse = await fetch(`${process.env.BASE_URL}/api/quizzes`, {
+      cache: 'no-cache',
+    })
+
+    if (!apiResponse.ok) {
+      console.log('Error fetching quiz', apiResponse)
+      return null
+    }
+
+    const quizJson = apiResponse.json()
+    return quizJson
+  } catch (e) {
+    console.log('Error fetching quiz', e)
+    return null
+  }
 }
